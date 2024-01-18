5c88cb312d1ab73fe3982c035060422d11c27efd
Agil875 <138016866+Agil875@users.noreply.github.com>
2024-01-18 16:41:29 +0400

    main-000 : work getBy queries

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,3 +8,23 @@ test("renders Salam", () => {
   expect(linkElement).toBeInTheDocument();
   expect(t).toBeInTheDocument();
 });
+
+describe("getByTesting", () => {
+  test("role", () => {
+    render(<App />);
+    const button = screen.getByRole("button", {
+      name: "Test button",
+    });
+    const linkTag = screen.getByRole("test_role");
+    const testByValue = screen.getByDisplayValue("test_value");
+    const byTestId = screen.getByTestId("custom_testid");
+    const allButton = screen.getAllByRole("button");
+    const test2Text = screen.getByText((c) => c.startsWith("Test2"));
+    expect(button).toBeInTheDocument();
+    expect(testByValue).toBeInTheDocument();
+    expect(linkTag).toBeInTheDocument();
+    expect(byTestId).toBeInTheDocument();
+    expect(allButton.length).toBe(3);
+    expect(test2Text).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,21 @@ const App = () => {
     <div>
       <h1>Salam</h1>
       <p>HI</p>
+      <button>Test button</button>
+      <button>Test2</button>
+      <section>
+        <a href="/">Go To second section</a>
+        <a
+          role="test_role"
+          href="/"
+          aria-label="test link"
+          aria-selected="true"
+        >
+          Go To second section
+        </a>
+      </section>
+      <input value="test_value" onChange={() => {}} />
+      <button data-testid="custom_testid">test id</button>
     </div>
   );
 };
